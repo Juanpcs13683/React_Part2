ea8e3d50720a1d5cc0ea22d970429c3f8ba416de
Juan Cañon <84336310+Juanpcs13683@users.noreply.github.com>
2022-09-27 22:52:50 +1000

    Course information until step 9 done
    Done all requirements of the course information all components in his own and all this part done.

--- courseinformation/src/components/Course.js
+++ courseinformation/src/components/Course.js
@@ -1,16 +1,18 @@
 import React from "react";
 import Header from "./Header";
 import Part from "./Part";
+import Total from "./Total";
 
 const Course = ({course}) =>{
     console.log()
+const total = course.parts.reduce((previous, current) => previous + current.exercises, 0)
     
     return(
         <div>
             <Header text={course.name} />
             {course.parts.map( part =>
-            <Part key={part.id} text={part.name}  value={part.exercises} />
-            ) }
+            <Part key={part.id} text={part.name}  value={part.exercises} />) }
+            <Total text="total of" value={total} text2="exercises"/>
         </div>
     )
 }
---
+++ courseinformation/src/components/Total.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Total = (props) => <b><p>{props.text} {props.value} {props.text2}</p></b>
+
+export default Total
