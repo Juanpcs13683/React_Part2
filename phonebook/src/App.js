f84bbd6d77fea7eee9e7fdd5d84090d9bcee1e31
Juan Cañon <84336310+Juanpcs13683@users.noreply.github.com>
2022-09-30 20:53:29 +1000

    Phonebook step3 done
    input for number phone added and inserted into the object persons with its own id.

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,15 +2,21 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('insert name...')
+  const [newPhone, setNewPhone] = useState('insert new phone')
 
  const addperson = (event) => {
   event.preventDefault()
 
   const personObject = {
-    name:newName
+    name:newName,
+    phone:newPhone,
+    id:persons.length+1
   }
 
   // this iteration let us figure out if the array 'persons' has already same value input in it
@@ -32,11 +38,16 @@ const App = () => {
 
  }
 
- //console.log(persons)
+ console.log(persons)
 
   const handlePersonChange = (event) => {
     //console.log(event.target.value)
     setNewName(event.target.value)
+    
+  }
+
+  const handlePhoneChange = (event) => {
+    setNewPhone(event.target.value)
   }
 
 
@@ -48,6 +59,9 @@ const App = () => {
       <form onSubmit={addperson}>
         <div>
           name: <input value={newName} onChange={handlePersonChange} />
+        </div>
+        <div>
+          number : <input value={newPhone}  onChange={handlePhoneChange}/>
         </div>
         <div>
           <button type="submit">add</button>
