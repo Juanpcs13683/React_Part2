87867df054548545e867070215c5621f49c7fb73
Juan Cañon <84336310+Juanpcs13683@users.noreply.github.com>
2022-10-04 01:27:14 +1100

    Creating server side in the apps

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,19 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+//we need to use a useEffect hook to get the data from the server, in this case the server
+//is json-server
+useEffect(() => {
+  console.log('effect')
+  axios.get('http://localhost:3001/persons').then(response => {
+    console.log('promise fulfilled')
+    setPersons(response.data)
+  })
+},[])
+
 
   //this part is used to add new persons
   const addPerson = (event) => {
--- renderingacollectionmodules/src/App.js
+++ renderingacollectionmodules/src/App.js
@@ -1,12 +1,26 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
+import axios from 'axios'
 import Note from "./components/Note"
 
 
-const App = (props) => {
+const App = () => {
 
-  const [notes, setNotes] = useState(props.notes)
+
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note ...')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+    .get('http://localhost:3001/notes')
+    .then(response => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    })
+},[])
+  console.log('render', notes.length, 'notes')
+
 
   const addNote = (event) => {
     event.preventDefault()
@@ -21,7 +35,7 @@ const App = (props) => {
     setNotes(notes.concat(noteObject))
     setNewNote('')
   }
-  console.log(notes)
+  //console.log(notes)
 
   const handleNoteChange = (event) => {
     console.log(event.target.value)
